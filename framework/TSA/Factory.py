ccc874b12c006e46a8254d05f319c708cad6f541
liyeni <li2181@purdue.edu>
2022-07-19 10:22:44 -0400

    update

--- framework/TSA/Factory.py
+++ framework/TSA/Factory.py
@@ -23,10 +23,12 @@ from .Fourier import Fourier
 from .ARMA import ARMA
 from .Wavelet import Wavelet
 from .PolynomialRegression import PolynomialRegression
+from .RWD import RWD
 
 factory = EntityFactory('TimeSeriesAnalyzer')
 # TODO map lower case to upper case, because of silly ROM namespace problems
 aliases = {'Fourier': 'fourier',
            'ARMA': 'arma',
-           'Wavelet': 'wavelet'}
+           'Wavelet': 'wavelet',
+           'RWD': 'rwd'}
 factory.registerAllSubtypes(TimeSeriesAnalyzer, alias=aliases)
